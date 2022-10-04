abdb5d94c05888acbbd94a9ad5d3db2ca33cb7c6
chelsbo <chelsea.reese@icloud.com>
2022-10-03 21:47:34 -0600

    Updated server and index

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('*notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
+// app.get('*notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/notes.html'));
+// });
 
 function createNewNote(body, notesArray) {
     const newNote = body;
