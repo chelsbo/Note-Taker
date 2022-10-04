afd66fbb575bf20833713c368e413c2d77662021
chelsbo <chelsea.reese@icloud.com>
2022-10-03 20:42:20 -0600

    Updated all files

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -12,17 +12,14 @@ if (window.location.pathname === '/notes') {
   noteList = document.querySelectorAll('.list-container .list-group');
 }
 
-// Show an element
 const show = (elem) => {
   elem.style.display = 'inline';
 };
 
-// Hide an element
 const hide = (elem) => {
   elem.style.display = 'none';
 };
 
-// activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
 const getNotes = () =>
@@ -77,9 +74,7 @@ const handleNoteSave = () => {
   });
 };
 
-// Delete the clicked note
 const handleNoteDelete = (e) => {
-  // prevents the click listener for the list from being called when the button inside of it is clicked
   e.stopPropagation();
 
   const note = e.target;
@@ -95,14 +90,12 @@ const handleNoteDelete = (e) => {
   });
 };
 
-// Sets the activeNote and displays it
 const handleNoteView = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
   renderActiveNote();
 };
 
-// Sets the activeNote to and empty object and allows the user to enter a new note
 const handleNewNoteView = (e) => {
   activeNote = {};
   renderActiveNote();
@@ -116,7 +109,6 @@ const handleRenderSaveBtn = () => {
   }
 };
 
-// Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
   if (window.location.pathname === '/notes') {
@@ -125,7 +117,6 @@ const renderNoteList = async (notes) => {
 
   let noteListItems = [];
 
-  // Returns HTML element with or without a delete button
   const createLi = (text, delBtn = true) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item');
@@ -133,6 +124,79 @@ const renderNoteList = async (notes) => {
     const spanEl = document.createElement('span');
     spanEl.innerText = text;
     spanEl.addEventListener('click', handleNoteView);
+
+
+    const inputVal = document.querySelector('.userInput');
+    const addTaskBtn = document.querySelector('.savebtn');
+    
+    function inputNoteVal(notesList) {
+        inputVal.value = notesList;
+    }
+
+    addTaskBtn.addEventListener('click', function() {    
+      if (inputVal.value != 0) {
+          let localItems = JSON.parse(localStorage.getItem('localItem'))
+  
+          if (localItems === null) {
+              taskList = []
+          } else {
+              taskList = localItems;
+          }
+          taskList.push(inputVal.value)
+          localStorage.setItem('localItem', JSON.stringify(taskList))
+      }
+      showList();
+      // resets input value 
+      inputVal.value = '';
+  })
+
+    var noteLists = [];
+
+    function showNotes () {
+    let outPut = '';
+    let taskListShow = document.querySelector('.noteListItems');  
+    let localItems = JSON.parse(localStorage.getItem('localItem'));
+ 
+    if (localItems === null) {
+      console.log('error')
+  } else {
+      taskList = localItems
+      console.log('success')
+  }
+
+  taskList.forEach((data, index) => {
+      outPut += `
+      <div class="notesList">
+          <p class="pText">${data}</p>
+          <button class="deleteTask" onClick="deleteItem(${index})">x</button>
+      </div>
+      `
+      console.log(data)
+  });
+  taskListShow.innerHTML = outPut;
+  // inputVal.value = '';
+}
+showNotes();
+
+
+    // will add the movie suggestion to the watch list
+    addTaskBtn.addEventListener('click', function() {    
+        if (inputVal.value != 0) {
+            let localItems = JSON.parse(localStorage.getItem('localItem'))
+    
+            if (localItems === null) {
+                taskList = []
+            } else {
+                taskList = localItems;
+            }
+            taskList.push(inputVal.value)
+            localStorage.setItem('localItem', JSON.stringify(taskList))
+        }
+        // calls the show list function
+        showNotes();
+        // resets input value 
+        // inputVal.value = '';
+    })
 
     liEl.append(spanEl);
 
@@ -169,7 +233,6 @@ const renderNoteList = async (notes) => {
   }
 };
 
-// Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
 if (window.location.pathname === '/notes') {
--- server.js
+++ server.js
@@ -27,6 +27,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('*notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
 function createNewNote(body, notesArray) {
     const newNote = body;
     if (!Array.isArray(notesArray))
